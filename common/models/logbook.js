26a53e2bf53a0566181182622b4b58df07a7bd0d
Henrik Johansson <henrik.johansson712@gmail.com>
2019-04-17 16:22:47 +0200

    Added findEntries query

--- common/models/logbook.js
+++ common/models/logbook.js
@@ -42,4 +42,30 @@ module.exports = function(Logbook) {
       },
     });
   };
+
+  /**
+   * Find all Logbook entries matching the query
+   * @param {string} name The name of the Logbook
+   * @param {string} query Query of content in Logbook entry
+   * @param {Function(Error, object)} callback
+   */
+
+  Logbook.findEntries = function(name, query) {
+    let Room = app.models.Room;
+    let pattern = new RegExp('.*' + query + '.*', 'i');
+    return Room.findOne({
+      where: {name: name},
+      fields: ['id', 'name'],
+      include: {
+        relation: 'messages',
+        scope: {
+          fields: ['timestamp', 'sender', 'content'],
+          order: 'timestamp ASC',
+          where: {
+            'content.body': {like: pattern},
+          },
+        },
+      },
+    });
+  };
 };
