9aec3eceea1a9ba55749eb720d840d9591ce9493
Henrik Johansson <henrik.johansson712@gmail.com>
2019-04-18 15:11:40 +0200

    Sync script is now set to run automatically every five minutes

---
+++ server/boot/sync.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const syncScript = require('../../sync-data/script');
+
+module.exports = function(app, cb) {
+  /*
+   * The `app` object provides access to a variety of LoopBack resources such as
+   * models (e.g. `app.models.YourModelName`) or data sources (e.g.
+   * `app.datasources.YourDataSource`). See
+   * https://loopback.io/doc/en/lb3/Working-with-LoopBack-objects.html
+   * for more info.
+   */
+  syncScript();
+  process.nextTick(cb); // Remove if you pass `cb` to an async function yourself
+};
--- sync-data/script.js
+++ sync-data/script.js
@@ -3,24 +3,32 @@
 const SyncData = require('./sync-data');
 const sync = new SyncData();
 
-sync
-  .syncRooms()
-  .then(roomResponse => {
-    console.log(roomResponse);
-    return sync.syncRoomEvents();
-  })
-  .then(roomEventResponse => {
-    console.log(roomEventResponse);
-    return sync.syncRoomMessages();
-  })
-  .then(roomMessageResponse => {
-    console.log(roomMessageResponse);
-    return sync.syncRoomMembers();
-  })
-  .then(roomMemberResponse => {
-    console.log(roomMemberResponse);
-    return sync.syncRoomImages();
-  })
-  .then(roomImageResponse => {
-    console.log(roomImageResponse);
-  });
+const INTERVAL = 300000; // ms
+
+module.exports = function syncScript() {
+  setInterval(() => {
+    console.log('Syncing...');
+    sync
+      .syncRooms()
+      .then(roomResponse => {
+        console.log(roomResponse);
+        return sync.syncRoomEvents();
+      })
+      .then(roomEventResponse => {
+        console.log(roomEventResponse);
+        return sync.syncRoomMessages();
+      })
+      .then(roomMessageResponse => {
+        console.log(roomMessageResponse);
+        return sync.syncRoomMembers();
+      })
+      .then(roomMemberResponse => {
+        console.log(roomMemberResponse);
+        return sync.syncRoomImages();
+      })
+      .then(roomImageResponse => {
+        console.log(roomImageResponse);
+        console.log('Sync complete');
+      });
+  }, INTERVAL);
+};
