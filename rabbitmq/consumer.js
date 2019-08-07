2941c57d40f8e115ee6349de95e7cda0e00f019b
Henrik Johansson <henrik.johansson712@gmail.com>
2019-08-07 09:57:41 +0200

    Fixed consumer error handling

--- rabbitmq/consumer.js
+++ rabbitmq/consumer.js
@@ -19,55 +19,61 @@ module.exports = function() {
 
     amqp.connect(url, function(error0, connection) {
       if (error0) {
-        throw error0;
-      }
-
-      connection.createChannel(function(error1, channel) {
-        if (error1) {
-          throw error1;
-        }
-
-        const queue = config.rabbitmq.queue;
-
-        channel.assertQueue(queue, {
-          durable: true,
-        });
-
-        channel.prefetch(1);
-
-        console.log(' [*] RABBITMQ Waiting for messages in %s', queue);
-
-        channel.consume(
-          queue,
-          async function(msg) {
-            let msgJSON = JSON.parse(msg.content);
-            switch (msgJSON.eventType) {
-              case 'PROPOSAL_ACCEPTED': {
-                console.log(' [/] Received %s', msg.content.toString());
-                try {
-                  const accessToken = await matrixClient.login(user, pass);
-                  const room = await matrixClient.createRoom(
-                    accessToken,
-                    msgJSON.proposalID,
-                    msgJSON.users
-                  );
-                  console.log(' [X] Created room %s', room.room_id);
-                } catch (err) {
-                  console.error(' [-] Error while creating room: %s', err);
-                }
-                channel.ack(msg);
-                break;
-              }
-              default:
-                channel.ack(msg);
-                break;
-            }
-          },
-          {
-            noAck: false,
-          }
+        console.error(
+          ' [-] Error while connecting to RabbitMq server: ',
+          error0
         );
-      });
+      } else {
+        connection.createChannel(function(error1, channel) {
+          if (error1) {
+            console.error(
+              ' [-] Error while creating RabbitMq channel: ',
+              error1
+            );
+          } else {
+            const queue = config.rabbitmq.queue;
+
+            channel.assertQueue(queue, {
+              durable: true,
+            });
+
+            channel.prefetch(1);
+
+            console.log(' [*] RABBITMQ Waiting for messages in %s', queue);
+
+            channel.consume(
+              queue,
+              async function(msg) {
+                let msgJSON = JSON.parse(msg.content);
+                switch (msgJSON.eventType) {
+                  case 'PROPOSAL_ACCEPTED': {
+                    console.log(' [/] Received %s', msg.content.toString());
+                    try {
+                      const accessToken = await matrixClient.login(user, pass);
+                      const room = await matrixClient.createRoom(
+                        accessToken,
+                        msgJSON.proposalID,
+                        msgJSON.users
+                      );
+                      console.log(' [X] Created room %s', room.room_id);
+                    } catch (err) {
+                      console.error(' [-] Error while creating room: %s', err);
+                    }
+                    channel.ack(msg);
+                    break;
+                  }
+                  default:
+                    channel.ack(msg);
+                    break;
+                }
+              },
+              {
+                noAck: false,
+              }
+            );
+          }
+        });
+      }
     });
   }
 };
