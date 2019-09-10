eca01c784dcef73cca4789d72e246c96de2ef8bf
Henrik Johansson <henrik.johansson712@gmail.com>
2019-09-10 15:06:06 +0200

    Synapse access token is now stored in db

--- common/models/logbook.js
+++ common/models/logbook.js
@@ -9,6 +9,29 @@ const user = config.synapse.bot.name;
 const pass = config.synapse.bot.password;
 
 module.exports = function(Logbook) {
+  const app = require('../../server/server');
+  let accessToken;
+
+  Logbook.beforeRemote('*', async function() {
+    try {
+      const AccessToken = app.models.AccessToken;
+      const tokenInstance = await AccessToken.findOne({where: {userId: user}});
+      if (tokenInstance && tokenInstance.userId === user) {
+        accessToken = tokenInstance.id;
+      } else {
+        accessToken = await matrixClient.login(user, pass);
+        const token = {
+          id: accessToken,
+          ttl: 1209600,
+          created: new Date(),
+          userId: user,
+        };
+        await AccessToken.create(token);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  });
   /**
    * Find Logbook model instance by name
    * @param {string} name Name of the Logbook
@@ -17,7 +40,6 @@ module.exports = function(Logbook) {
 
   Logbook.findByName = async function(name) {
     try {
-      const accessToken = await matrixClient.login(user, pass);
       const room = await matrixClient.fetchRoomByName(name, accessToken);
       return await matrixClient.fetchRoomMessages(room, accessToken);
     } catch (err) {
@@ -32,7 +54,6 @@ module.exports = function(Logbook) {
 
   Logbook.findAll = async function() {
     try {
-      const accessToken = await matrixClient.login(user, pass);
       const rooms = await matrixClient.fetchRooms(accessToken);
       return await matrixClient.fetchAllRoomsMessages(rooms, accessToken);
     } catch (err) {
@@ -49,7 +70,6 @@ module.exports = function(Logbook) {
 
   Logbook.filter = async function(name, filter) {
     try {
-      const accessToken = await matrixClient.login(user, pass);
       const room = await matrixClient.fetchRoomByName(name, accessToken);
       return await matrixClient.fetchRoomMessages(room, accessToken, filter);
     } catch (err) {
