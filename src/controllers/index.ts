898cdc45d772bb2c641633e1c7fe872fe1dcc484
Henrik Johansson <henrik.johansson712@gmail.com>
2021-03-29 14:35:35 +0200

    Add Synapse controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,1 +1,2 @@
-export * from './ping.controller';
+export * from "./ping.controller";
+export * from "./synapse.controller";
---
+++ src/controllers/synapse.controller.ts
@@ -0,0 +1,7 @@
+// Uncomment these imports to begin using these cool features!
+
+// import {inject} from '@loopback/context';
+
+export class SynapseController {
+  constructor() {}
+}
