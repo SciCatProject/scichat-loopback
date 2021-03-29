805b1a683e768c13500c8a44be6f6948ed2363e8
Henrik Johansson <henrik.johansson712@gmail.com>
2021-03-29 14:39:27 +0200

    Add login method to Synapse controller

--- src/controllers/synapse.controller.ts
+++ src/controllers/synapse.controller.ts
@@ -1,7 +1,15 @@
-// Uncomment these imports to begin using these cool features!
-
-// import {inject} from '@loopback/context';
+import { inject } from "@loopback/context";
+import { param, post } from "@loopback/openapi-v3";
+import { Synapse } from "../services";
 
 export class SynapseController {
-  constructor() {}
+  constructor(@inject("services.Synapse") protected synapseService: Synapse) {}
+
+  @post("/login")
+  async login(
+    @param.path.string("username") username: string,
+    @param.path.password("password") password: string,
+  ): Promise<object> {
+    return this.synapseService.login(username, password);
+  }
 }
