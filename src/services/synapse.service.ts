a64a91521c050400139d5fc662bc508b7ab20bad
Henrik Johansson <henrik.johansson712@gmail.com>
2021-03-29 15:50:52 +0200

    Add remaining methods to Synapse service

--- src/services/synapse.service.ts
+++ src/services/synapse.service.ts
@@ -2,9 +2,25 @@ import { inject, Provider } from "@loopback/core";
 import { getService } from "@loopback/service-proxy";
 import { SynapseDataSource } from "../datasources";
 
+export interface Message {
+  message: string;
+}
 export interface Synapse {
   login(username: string, password: string): Promise<object>;
+  createRoom(
+    accessToken: string,
+    name: string,
+    invite?: string[],
+  ): Promise<object>;
+  fetchAllRoomsMessages(accessToken: string, options?: object): Promise<object>;
+  fetchRoomIdByName(name: string): Promise<object>;
+  fetchRoomMessages(accessToken: string, options?: object): Promise<object>;
   fetchPublicRooms(accessToken: string): Promise<object>;
+  sendMessage(
+    accessToken: string,
+    roomId: string,
+    data: Message,
+  ): Promise<object>;
 }
 
 export class SynapseProvider implements Provider<Synapse> {
