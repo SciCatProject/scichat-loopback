469bba3b6bdcbb8cf112718bfe6ecffb4e16c5b7
Henrik Johansson <henrik.johansson712@gmail.com>
2021-03-30 13:54:42 +0200

    Add synapse create room response model

---
+++ src/models/synapse-create-room-response.model.ts
@@ -0,0 +1,25 @@
+import {Model, model, property} from '@loopback/repository';
+
+@model()
+export class SynapseCreateRoomResponse extends Model {
+  @property({
+    type: 'string',
+  })
+  room_alias?: string;
+
+  @property({
+    type: 'string',
+  })
+  room_id?: string;
+
+
+  constructor(data?: Partial<SynapseCreateRoomResponse>) {
+    super(data);
+  }
+}
+
+export interface SynapseCreateRoomResponseRelations {
+  // describe navigational properties here
+}
+
+export type SynapseCreateRoomResponseWithRelations = SynapseCreateRoomResponse & SynapseCreateRoomResponseRelations;
