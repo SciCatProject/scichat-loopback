f95a18170d6694957a6cf4974f24a0f7e494c6aa
Henrik Johansson <henrik.johansson712@gmail.com>
2021-03-30 11:07:36 +0200

    Add interfaces for synapse fethRoomMessages

--- src/models/index.ts
+++ src/models/index.ts
@@ -4,8 +4,37 @@ export * from "./synapse-login-response.model";
 export * from "./synapse-room-id-response.model";
 export * from "./synapse-sync-response.model";
 
+export interface SynapseTimelineEvent {
+  type: string;
+  sender: string;
+  content: {
+    msgtype: string;
+    body: string;
+    url?: string;
+  };
+  origin_server_ts: number;
+  unsigned: {
+    age: number;
+  };
+  event_id: string;
+}
+export interface SynapseJoinedRoom {
+  timeline: {
+    events: object[];
+  };
+  state: {
+    events: SynapseTimelineEvent[];
+  };
+  account_data: unknown[];
+  ephemeral: unknown[];
+  unread_notifications: {
+    notification_count: number;
+    highlight_count: number;
+  };
+  summary: unknown;
+}
 export interface SynapseRooms {
   invite: Record<string, unknown>;
-  join: Record<string, unknown>;
+  join: Record<string, SynapseJoinedRoom>;
   leave: Record<string, unknown>;
 }
