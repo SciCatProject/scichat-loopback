650cba1644fe2e0df110778f287f8b9fc3202f19
Henrik Johansson <henrik.johansson712@gmail.com>
2021-03-30 11:08:21 +0200

    Set up basic Logbook findByName endpoint

--- src/controllers/logbook.controller.ts
+++ src/controllers/logbook.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/camelcase */
 import { inject } from "@loopback/context";
 import { get, getModelSchemaRef, param } from "@loopback/openapi-v3";
 import { Logbook } from "../models";
@@ -26,12 +27,33 @@ export class LogbookController {
     },
   })
   async findByName(@param.path.string("name") name: string): Promise<object> {
-    // const accessToken = this.synapseService.login("username", "password");
-    const roomInfo = await this.synapseService.fetchRoomIdByName(name);
-    return roomInfo;
-    // return this.synapseService.fetchRoomMessages(
-    //   roomInfo["room_id"],
-    //   accessToken,
-    // );
+    const { access_token: accessToken } = await this.synapseService.login(
+      "username",
+      "password",
+    );
+    const roomAlias = encodeURIComponent(`#${name}:ess`);
+    const { room_id: roomId } = await this.synapseService.fetchRoomIdByName(
+      roomAlias,
+    );
+    const filter = encodeURIComponent(
+      JSON.stringify({
+        account_data: { not_types: ["m.*", "im.*"] },
+        presence: { not_types: ["*"] },
+        room: {
+          rooms: [roomId],
+          state: { types: ["m.room.name"] },
+          timeline: {
+            limit: 1000000,
+            types: ["m.room.message"],
+          },
+        },
+      }),
+    );
+    const { rooms } = await this.synapseService.fetchRoomMessages(
+      filter,
+      accessToken,
+    );
+    const messages = rooms.join[roomId].timeline.events;
+    return { roomId, name, messages };
   }
 }
