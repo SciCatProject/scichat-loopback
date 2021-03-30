c9f0b03ab8193ae621f701291fd52291378df032
Henrik Johansson <henrik.johansson712@gmail.com>
2021-03-30 13:34:10 +0200

    Add findAll endpoint to Logbook

--- src/controllers/logbook.controller.ts
+++ src/controllers/logbook.controller.ts
@@ -7,6 +7,64 @@ import { Synapse } from "../services";
 export class LogbookController {
   constructor(@inject("services.Synapse") protected synapseService: Synapse) {}
 
+  private username = "";
+  private password = "";
+
+  @get("/Logbooks", {
+    parameters: [
+      {
+        name: "filter",
+        schema: { title: "Filter", type: "string" },
+        in: "query",
+      },
+    ],
+    responses: {
+      "200": {
+        description: "Array of Logbook model instances",
+        content: {
+          "application/json": {
+            schema: [getModelSchemaRef(Logbook)],
+          },
+        },
+      },
+    },
+  })
+  async findAll(): Promise<Logbook[]> {
+    const { access_token: accessToken } = await this.synapseService.login(
+      this.username,
+      this.password,
+    );
+    const filter = encodeURIComponent(
+      JSON.stringify({
+        account_data: { not_types: ["m.*", "im.*"] },
+        presence: { not_types: ["*"] },
+        room: {
+          state: { types: ["m.room.name"] },
+          timeline: {
+            limit: 1000000,
+            types: ["m.room.message"],
+          },
+        },
+      }),
+    );
+    const { rooms } = await this.synapseService.fetchAllRoomsMessages(
+      filter,
+      accessToken,
+    );
+    return Object.keys(rooms.join)
+      .map(
+        (roomId) =>
+          new Logbook({
+            roomId,
+            name: rooms.join[roomId].state.events
+              .map((event) => event.content["name"])
+              .pop(),
+            messages: rooms.join[roomId].timeline.events,
+          }),
+      )
+      .filter((room) => room.roomId && room.name && room.messages);
+  }
+
   @get("/Logbooks/{name}", {
     parameters: [
       {
@@ -26,10 +84,10 @@ export class LogbookController {
       },
     },
   })
-  async findByName(@param.path.string("name") name: string): Promise<object> {
+  async findByName(@param.path.string("name") name: string): Promise<Logbook> {
     const { access_token: accessToken } = await this.synapseService.login(
-      "username",
-      "password",
+      this.username,
+      this.password,
     );
     const roomAlias = encodeURIComponent(`#${name}:ess`);
     const { room_id: roomId } = await this.synapseService.fetchRoomIdByName(
@@ -54,6 +112,6 @@ export class LogbookController {
       accessToken,
     );
     const messages = rooms.join[roomId].timeline.events;
-    return { roomId, name, messages };
+    return new Logbook({ roomId, name, messages });
   }
 }
