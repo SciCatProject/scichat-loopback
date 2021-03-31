c42b8cfce73b8b3255e5da39c5c2f6378e5628e9
Henrik Johansson <henrik.johansson712@gmail.com>
2021-03-31 13:30:58 +0200

    Add user controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,2 +1,3 @@
 export * from "./logbook.controller";
 export * from "./ping.controller";
+export * from "./user.controller";
---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,7 @@
+// Uncomment these imports to begin using these cool features!
+
+// import {inject} from '@loopback/core';
+
+export class UserController {
+  constructor() {}
+}
