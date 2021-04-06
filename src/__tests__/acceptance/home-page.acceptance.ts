bf10222ea6db1fc5e58d32dc3a43672d6a3212ac
Henrik Johansson <henrik.johansson712@gmail.com>
2021-04-06 16:45:57 +0200

    Update explorer link on home page

--- src/__tests__/acceptance/home-page.acceptance.ts
+++ src/__tests__/acceptance/home-page.acceptance.ts
@@ -1,31 +1,31 @@
-import {Client} from '@loopback/testlab';
-import {ScichatLoopbackApplication} from '../..';
-import {setupApplication} from './test-helper';
+import { Client } from "@loopback/testlab";
+import { ScichatLoopbackApplication } from "../..";
+import { setupApplication } from "./test-helper";
 
-describe('HomePage', () => {
+describe("HomePage", () => {
   let app: ScichatLoopbackApplication;
   let client: Client;
 
-  before('setupApplication', async () => {
-    ({app, client} = await setupApplication());
+  before("setupApplication", async () => {
+    ({ app, client } = await setupApplication());
   });
 
   after(async () => {
     await app.stop();
   });
 
-  it('exposes a default home page', async () => {
+  it("exposes a default home page", async () => {
     await client
-      .get('/')
+      .get("/")
       .expect(200)
-      .expect('Content-Type', /text\/html/);
+      .expect("Content-Type", /text\/html/);
   });
 
-  it('exposes self-hosted explorer', async () => {
+  it("exposes self-hosted explorer", async () => {
     await client
-      .get('/explorer/')
+      .get("/scichatexplorer/")
       .expect(200)
-      .expect('Content-Type', /text\/html/)
+      .expect("Content-Type", /text\/html/)
       .expect(/<title>LoopBack API Explorer/);
   });
 });
