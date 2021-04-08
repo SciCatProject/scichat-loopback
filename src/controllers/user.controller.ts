c61ae19c9bcf29f6891cdc3ecc1ec57288b2c787
Henrik Johansson <henrik.johansson712@gmail.com>
2021-04-08 16:27:10 +0200

    Remove User.whoAmI endpoint

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,15 +1,14 @@
-import { authenticate, TokenService } from "@loopback/authentication";
+import { TokenService } from "@loopback/authentication";
 import { inject } from "@loopback/core";
 import { model, property, repository } from "@loopback/repository";
 import {
   api,
-  get,
   getModelSchemaRef,
   post,
   requestBody,
   SchemaObject,
 } from "@loopback/rest";
-import { SecurityBindings, securityId, UserProfile } from "@loopback/security";
+import { SecurityBindings, UserProfile } from "@loopback/security";
 import { genSalt, hash } from "bcryptjs";
 import _ from "lodash";
 import { TokenServiceBindings, UserServiceBindings } from "../keys";
@@ -83,27 +82,6 @@ export class UserController {
     const userProfile = this.userService.convertToUserProfile(user);
     const token = await this.jwtService.generateToken(userProfile);
     return { token };
-  }
-
-  @authenticate("jwt")
-  @get("/Users/whoAmI", {
-    responses: {
-      "200": {
-        description: "Return current user",
-        content: {
-          "application/json": {
-            schema: {
-              type: "string",
-            },
-          },
-        },
-      },
-    },
-  })
-  async whoAmI(
-    @inject(SecurityBindings.USER) currentUserProfile: UserProfile,
-  ): Promise<string> {
-    return currentUserProfile[securityId];
   }
 
   @post("/Users", {
