6038148f8e23a76fd995a98b845f2485bbe3ec18
Henrik Johansson <henrik.johansson712@gmail.com>
2021-04-08 16:28:34 +0200

    Remove RefreshToken and UserCredentials controllers

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,5 +1,3 @@
 export * from "./logbook.controller";
 export * from "./ping.controller";
 export * from "./user.controller";
-export * from './refresh-token-user.controller';
-export * from './user-user-credentials.controller';
