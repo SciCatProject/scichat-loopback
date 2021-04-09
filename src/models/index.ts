59975de18615ab0aa673b5b4eda3af29d78dc820
Henrik Johansson <henrik.johansson712@gmail.com>
2021-04-09 14:36:48 +0200

    Add SynapseToken model

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,7 @@
 export * from "./logbook.model";
 export * from "./synapse-login-response.model";
 export * from "./synapse-sync-response.model";
+export * from "./synapse-token.model";
 export * from "./user-credentials.model";
 export * from "./user.model";
 
---
+++ src/models/synapse-token.model.ts
@@ -0,0 +1,52 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class SynapseToken extends Entity {
+  @property({
+    type: 'string',
+    required: true,
+  })
+  user_id: string;
+
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  access_token: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  home_server: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  device_id: string;
+
+  @property({
+    type: 'object',
+    required: true,
+  })
+  well_known: object;
+
+
+  constructor(data?: Partial<SynapseToken>) {
+    super(data);
+  }
+}
+
+export interface SynapseTokenRelations {
+  // describe navigational properties here
+}
+
+export type SynapseTokenWithRelations = SynapseToken & SynapseTokenRelations;
