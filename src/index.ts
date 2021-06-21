9929cbdb1d08d87cee31a8347931d437b307129e
Henrik Johansson <henrik.johansson712@gmail.com>
2021-06-21 11:22:08 +0200

    Set up duo-message-broker consumer

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
+import { Queue, RabbitMQMessageBroker } from "@esss-swap/duo-message-broker";
 import { genSalt, hash } from "bcryptjs";
 import { ApplicationConfig, ScichatLoopbackApplication } from "./application";
 import { UserRepository } from "./repositories";
+import { Utils } from "./utils";
 
 export * from "./application";
 export * from "./jwt-authentication-component";
 export * from "./keys";
+
+export interface Member {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
+export interface ProposalAcceptedMessage {
+  proposalId: number;
+  shortCode: string;
+  title: string;
+  members: Member[];
+  proposer?: Member;
+}
 
 export async function main(options: ApplicationConfig = {}) {
   const app = new ScichatLoopbackApplication(options);
@@ -34,6 +50,65 @@ export async function main(options: ApplicationConfig = {}) {
     await userRepository
       .userCredentials(savedUser.id)
       .create({ password: hashedPassword });
+  }
+
+  // Set up RabbitMQ
+  const rabbitMqEnabled = process.env.RABBITMQ_ENABLED ?? "no";
+  if (rabbitMqEnabled === "yes") {
+    const rabbitMq = new RabbitMQMessageBroker();
+
+    await rabbitMq.setup({
+      hostname: process.env.RABBITMQ_HOST ?? "localhost",
+      username: process.env.RABBITMQ_USER ?? "rabbitmq",
+      password: process.env.RABBITMQ_PASSWORD ?? "rabbitmq",
+    });
+
+    rabbitMq.listenOn(Queue.PROPOSAL, async (type, message: unknown) => {
+      if (type === "PROPOSAL_ACCEPTED") {
+        console.log("PROPOSAL_ACCEPTED: ", message);
+        const proposalAcceptedMessage: ProposalAcceptedMessage = message as ProposalAcceptedMessage;
+        const members: Member[] = proposalAcceptedMessage.members;
+        if (proposalAcceptedMessage.proposer) {
+          members.push(proposalAcceptedMessage.proposer);
+        }
+
+        do {
+          try {
+            const membersToCreate = await Utils.prototype.membersToCreate(
+              members,
+            );
+            await Promise.all(
+              membersToCreate.map(async (member) =>
+                Utils.prototype.createUser(member),
+              ),
+            );
+            const invites = members.map(
+              (member) =>
+                member.firstName.toLowerCase() + member.lastName.toLowerCase(),
+            );
+            const logbookDetails = await Utils.prototype.createRoom(
+              proposalAcceptedMessage.shortCode,
+              invites,
+            );
+            console.log("Room created with details: ", logbookDetails);
+          } catch (err) {
+            if (
+              err.error &&
+              (err.error.errcode === "M_UNKNOWN_TOKEN" ||
+                err.error.errcode === "M_MISSING_TOKEN")
+            ) {
+              await Utils.prototype.renewAccessToken();
+              continue;
+            } else {
+              console.error(err);
+            }
+          }
+          break;
+        } while (true);
+      } else {
+        throw new Error("Received unknown event");
+      }
+    });
   }
 
   return app;
