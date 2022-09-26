63eb830546a94f2805a5032a4745df28797d405a
Martin Trajanovski <martin.trajanovski@gmail.com>
2022-09-26 14:30:32 +0200

    add the synapse token check and creation before message logic

--- src/observers/rabbit-mq.observer.ts
+++ src/observers/rabbit-mq.observer.ts
@@ -1,9 +1,13 @@
 import { inject, lifeCycleObserver, LifeCycleObserver } from "@loopback/core";
+import { repository } from "@loopback/repository";
 import {
   Queue,
   RabbitMQMessageBroker,
 } from "@user-office-software/duo-message-broker";
+import _ from "lodash";
 import { Member, ProposalMessageData } from "..";
+import { SynapseTokenRepository } from "../repositories";
+import { SynapseService } from "../services";
 import { Utils } from "../utils";
 
 const proposalStatusTrigger =
@@ -15,7 +19,16 @@ const proposalStatusTrigger =
  */
 @lifeCycleObserver("")
 export class RabbitMqObserver implements LifeCycleObserver {
-  constructor(@inject("utils") protected utils: Utils) {}
+  constructor(
+    @inject("utils") protected utils: Utils,
+    @repository(SynapseTokenRepository)
+    public synapseTokenRepositry: SynapseTokenRepository,
+    @inject("services.Synapse") protected synapseService: SynapseService,
+  ) {}
+
+  username = process.env.SYNAPSE_BOT_NAME ?? "";
+  password = process.env.SYNAPSE_BOT_PASSWORD ?? "";
+  serverName = process.env.SYNAPSE_SERVER_NAME ?? "ess";
 
   /**
    * This method will be invoked when the application initializes. It will be
@@ -23,6 +36,37 @@ export class RabbitMqObserver implements LifeCycleObserver {
    */
   async init(): Promise<void> {
     // Add your logic for init
+  }
+
+  async checkForSynapseTokenOrCreateOne() {
+    try {
+      // Add pre-invocation logic here
+      console.log("Looking for Synapse token in database");
+      const userId = `@${this.username}:${this.serverName}`;
+      const tokenInstance = await this.synapseTokenRepositry.findOne({
+        where: { user_id: userId },
+      });
+      if (tokenInstance && tokenInstance.user_id === userId) {
+        console.log("Found Synapse token", {
+          synapseToken: tokenInstance.access_token,
+        });
+      } else {
+        console.log("Synapse token not found, requesting new token");
+        const synapseLoginResponse = await this.synapseService.login(
+          this.username,
+          this.password,
+        );
+        await this.synapseTokenRepositry.create(
+          _.omit(synapseLoginResponse, "well_known"),
+        );
+        console.log("Request for new access token successful", {
+          synapseToken: synapseLoginResponse.access_token,
+        });
+      }
+    } catch (err) {
+      console.error(err);
+      // Add error handling logic here
+    }
   }
 
   /**
@@ -45,6 +89,12 @@ export class RabbitMqObserver implements LifeCycleObserver {
           switch (type) {
             case "PROPOSAL_STATUS_CHANGED_BY_WORKFLOW":
             case "PROPOSAL_STATUS_CHANGED_BY_USER": {
+              /**
+               * TODO: This should be solved a bit better because there is already an interceptor for this.
+               * When we use the RabbitMQ messages we call Util functions directly and completely skipping the interceptor
+               * Now the interceptor logic is duplicated here and used to check for token every time we receive a new message on the queue.
+               */
+              await this.checkForSynapseTokenOrCreateOne();
               console.log("Message type ", type);
               console.log("Message content: ", message);
 
@@ -53,6 +103,8 @@ export class RabbitMqObserver implements LifeCycleObserver {
                 console.log(
                   `Non trigger status ${proposalStatusTrigger}. Nothing to do`,
                 );
+
+                return;
               }
 
               const members: Member[] = proposalMessage.members;
