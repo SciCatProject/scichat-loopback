d36c1494950e0fc4b7fb97253fa8fbd927cac60c
Jay Quan <a331998513@gmail.com>
2023-09-07 10:13:10 +0200

    test

--- src/__tests__/acceptance/logbook.controller.acceptance.ts
+++ src/__tests__/acceptance/logbook.controller.acceptance.ts
@@ -15,32 +15,35 @@ describe("LogbookController (acceptance)", () => {
   });
 
   context("find", () => {
-    it("should resolve in a 401 code with unauthenticated user", async () => {
+    it("should resolve in a 401 code with unauthenticated user", async (done) => {
       try {
         await client.get("/scichatapi/Logbooks");
       } catch (error) {
         expect(error.statusCode).equal(401);
       }
+      done();
     });
   });
 
   context("findByName", () => {
-    it("should resolve in a 401 code with unauthenticated user", async () => {
+    it("should resolve in a 401 code with unauthenticated user", async (done) => {
       try {
         await client.get("/scichatapi/Logbooks/123456");
       } catch (error) {
         expect(error.statusCode).equal(401);
       }
+      done();
     });
   });
 
   context("sendMessage", () => {
-    it("should resolve in a 401 code with unauthenticated user", async () => {
+    it("should resolve in a 401 code with unauthenticated user", async (done) => {
       try {
         await client.post("/scichatapi/Logbooks/123456/message");
       } catch (error) {
         expect(error.statusCode).equal(401);
       }
+      done();
     });
   });
 });
