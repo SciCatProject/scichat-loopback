fa251fd56f042e26807956ef615aa138914216ad
Jay Quan <a331998513@gmail.com>
2023-09-10 15:23:11 +0200

    test

--- src/__tests__/acceptance/logbook.controller.acceptance.ts
+++ src/__tests__/acceptance/logbook.controller.acceptance.ts
@@ -4,8 +4,6 @@ import { setupApplication } from "./test-helper";
 describe("LogbookController (acceptance)", () => {
   let app: ScichatLoopbackApplication;
   let client: Client;
-  const testDone = (ms = 0) =>
-    new Promise((resolve) => setTimeout(resolve, 2000));
   before("setupApplication", async () => {
     ({ app, client } = await setupApplication());
   });
@@ -16,16 +14,21 @@ describe("LogbookController (acceptance)", () => {
 
   context("find", () => {
     it("should resolve in a 401 code with unauthenticated user", async function () {
-      const result = await client.get("/scichatapi/Logbooks");
-      expect(result.statusCode).equal(401);
-      await testDone();
-    }).timeout(10000);
+      try {
+        await client.get("/scichatapi/Logbooks");
+      } catch (err) {
+        expect(err.statusCode).equal(401);
+      }
+    });
   });
 
   context("findByName", () => {
     it("should resolve in a 401 code with unauthenticated user", async () => {
-      const result = await client.get("/scichatapi/Logbooks/123456");
-      expect(result.statusCode).equal(401);
+      try {
+        await client.get("/scichatapi/Logbooks/123456");
+      } catch (err) {
+        expect(err.statusCode).equal(401);
+      }
     });
   });
 
